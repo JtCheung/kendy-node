07ba2e91ea59a0a0f179b2729fae229c99154853
zhangjunteng1 <zhangjunteng1@joyy.com>
2023-03-05 16:56:59 +0800

    feat: 获取资料列表

--- src/pages/api/handler.ts
+++ src/pages/api/handler.ts
@@ -5,42 +5,76 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const { name, ...params } = request.body;
 
-  if (name === 'getChatMsg') {
-    const res = await axios.post(
-      'https://live.azavt.com/chatroom/v2/msgPage',
-      {
-        pageSize: 20,
-        orderby: 'SEND_TIMESTAMP~DESC',
-        isManager: '',
-        fromMemberId: '',
-        riskChat: '',
-        ...params,
-      },
-      {
-        headers: getHeaders(),
-      },
-    );
+  switch (name) {
+    case 'getChatMsg': {
+      const res = await axios.post(
+        'https://live.azavt.com/chatroom/v2/msgPage',
+        {
+          pageSize: 20,
+          orderby: 'SEND_TIMESTAMP~DESC',
+          isManager: '',
+          fromMemberId: '',
+          riskChat: '',
+          ...params,
+        },
+        {
+          headers: getHeaders(),
+        },
+      );
 
-    return response.status(200).json(res.data);
+      return response.status(200).json(res.data);
+    }
+    case 'getHistoryChatMsg': {
+      const res = await axios.post(
+        'https://live.azavt.com/chatroom/v2/historyMsgPage',
+        {
+          pageSize: 20,
+          orderby: 'SEND_TIMESTAMP~ASC',
+          isManager: '',
+          fromMemberId: '',
+          riskChat: '',
+          ...params,
+        },
+        {
+          headers: getHeaders(),
+        },
+      );
+      return response.status(200).json(res.data);
+    }
+
+    case 'getHistoryChatMsg': {
+      const res = await axios.post(
+        'https://live.azavt.com/chatroom/v2/historyMsgPage',
+        {
+          pageSize: 20,
+          orderby: 'SEND_TIMESTAMP~ASC',
+          isManager: '',
+          fromMemberId: '',
+          riskChat: '',
+          ...params,
+        },
+        {
+          headers: getHeaders(),
+        },
+      );
+      return response.status(200).json(res.data);
+    }
+
+    case 'getInfoList': {
+      const res = await axios.post(
+        'https://live.azavt.com/chatroom/file/list',
+        {},
+        {
+          params: {
+            chatRoomId: params.chatRoomId,
+          },
+          headers: getHeaders(),
+        },
+      );
+      return response.status(200).json(res.data);
+    }
+
+    default:
+      return response.status(200).send('hi!');
   }
-
-  if (name === 'getHistoryChatMsg') {
-    const res = await axios.post(
-      'https://live.azavt.com/chatroom/v2/historyMsgPage',
-      {
-        pageSize: 20,
-        orderby: 'SEND_TIMESTAMP~ASC',
-        isManager: '',
-        fromMemberId: '',
-        riskChat: '',
-        ...params,
-      },
-      {
-        headers: getHeaders(),
-      },
-    );
-    return response.status(200).json(res.data);
-  }
-
-  return response.status(200).send('hi!');
 }
